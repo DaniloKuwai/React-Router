0f01a6976ba54199e9b17e1d99126d25e006ace2
Danilo Ryoji Kuwai <danilo.kuwai@cobasi.com.br>
2023-01-23 17:07:03 -0300

    aula 02 - Aplicando estilo

---
+++ navegacao/src/components/layout/Menu.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Menu.css'
+
+export default function Menu(props){
+    return (
+            <aside className="Menu">
+            Componente Menu
+            </aside>
+    )
+}
--- navegacao/src/index.js
+++ navegacao/src/index.js
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom'
 import React from "react"
+import App from './views/examples/App'
 
 ReactDOM.render(
-    <h1>Olá React</h1>,
+    <h1><App></App></h1>,
     document.getElementById('root')
 )
