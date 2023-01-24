1fef23d65fa793a781b575f4cedd348256dbac04
Danilo Ryoji Kuwai <danilo.kuwai@cobasi.com.br>
2023-01-24 17:52:09 -0300

    aula 04 nao esta funcionando revisar o motivo

--- navegacao/src/components/layout/Content.jsx
+++ navegacao/src/components/layout/Content.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import './Content.css'
+import { Routes, Route } from 'react-router-dom'
+
+import About from '../../views/examples/About'
+import Home from '../../views/examples/Home'
 
 export default function Content(props){
     return (
             <main className="Content">
-            <h1>Componente Content</h1>
+            <Routes>
+                <Route path="/about" element={<About />}/>
+                <Route path="/home" element={<Home/>}/>
+            </Routes>
             </main>
     )
 }
--- navegacao/src/views/examples/App.jsx
+++ navegacao/src/views/examples/App.jsx
@@ -3,11 +3,15 @@ import Content from "../../components/layout/Content";
 import Menu from "../../components/layout/Menu";
 import './App.css'
 
+import { BrowserRouter } from 'react-router-dom'
+
 export default function App(props){
     return (
         <div className="App">
+            <BrowserRouter>
             <Menu></Menu>
             <Content></Content>
+            </BrowserRouter>
         </div>
     )
 }
